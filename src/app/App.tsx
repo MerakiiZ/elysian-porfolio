59190e1cccdaf18971ccf9b22eec5fdd38ab907f
MerakiiZ <christlezoe4@gmail.com>
2025-09-27 07:36:55 +0800

    feat: add FeaturedArt component, include new image assets, and update package dependencies

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,13 +4,15 @@ import Navbar from '../components/Navbar.tsx'
 import Hero from '../components/Hero.tsx'
 import Footer from '../components/Footer.tsx'
 import TestContent from '../components/TestContent.tsx'
+import FeaturedArt from '../components/FeaturedArt.tsx'
 
 function App() {
   return (
     <main className="relative min-h-screen bg-elysian1">
        <div className="flex flex-col gap-32">
         <Navbar />
-        <Hero />
+        <Hero />.
+        <FeaturedArt />
         <TestContent />
         <Footer />
       </div>
---
+++ src/components/FeaturedArt.tsx
@@ -0,0 +1,22 @@
+import "../app/global.css"
+
+export default function FeaturedArt(){
+    return (
+    <section>
+    <div>
+        <img
+        src="images/sera.webp"
+        alt="Sera art"
+        className=""
+        ></img>
+    </div>
+    
+    <div className="top-[40px] z-5">
+        <img
+        src="assets/feat_art_namecard.png"
+        className=""
+        ></img>
+    </div>
+    </section>
+)
+}
